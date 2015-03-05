30fb6e201e32b4158afb20971827f0fc7947ae87
kasey <kaseyk@gmail.com>
2015-03-05 02:24:05 -0500

    Install virtualbox, put packer in vagrant user dir

--- install.sh
+++ install.sh
@@ -18,12 +18,18 @@ sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
 # fail asap if sed blew it
 sudo date
 
+# install virtualbox
+sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free' > /etc/apt/sources.list.d/virtualbox.list"
+wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
+sudo apt-get update
+sudo apt-get -y -q install virtualbox-4.3 dkms
+
 # download the packer binary
 wget -q -O ~/packer.zip https://dl.bintray.com/mitchellh/packer/packer_0.7.5_linux_amd64.zip
 
 # unzip to the 'packer' directory in homedir and cleanup .zip
-unzip ~/packer.zip -d ~/packer
+unzip ~/packer.zip -d /home/vagrant/packer
 rm ~/packer.zip
 
 # run the command to verify installation
-~/packer/packer version
+/home/vagrant/packer/packer version
